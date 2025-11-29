505e6f7d1ae62849b5478fed76710464e2fa16a8
Ravinder <ravinder25886@gmail.com>
2025-11-29 20:26:49 +0000

    fix: removed warnings

--- RSFramework/HttpClientFactoryServiceAPI/Controllers/TestApiController.cs
+++ RSFramework/HttpClientFactoryServiceAPI/Controllers/TestApiController.cs
@@ -92,8 +92,8 @@ public class Post
 {
     public int UserId { get; set; }
     public int Id { get; set; }
-    public string Title { get; set; }
-    public string Body { get; set; }
+    public required string Title { get; set; }
+    public required string Body { get; set; }
 }
 public class CreatePostModel
 {
